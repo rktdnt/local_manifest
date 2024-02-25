d4a6f63fcbf3c227d5a5a1bb9c7e3c362b54764f
Raya Mahardika <rayapkun@gmail.com>
2024-02-25 21:47:35 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 #Credit to Meghthedev for the initial script 
 
 # Initialize repo with specified manifest
-repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21 --git-lfs
+repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 crave set --projectID 36
 
 # Run inside foss.crave.io devspace, in the project folder
