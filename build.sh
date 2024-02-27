13e6db5e5b8cfffa9c56eb790d073e01ed05fda6
Raya Mahardika <rayapkun@gmail.com>
2024-02-27 21:42:08 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,35 +5,9 @@ set -e
 
 # Initialize repo with specified manifest
 repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
+crave set --projectID 72
 
-# Run inside foss.crave.io devspace, in the project folder
-# Remove existing local_manifests
-crave run --no-patch -- "rm -rf .repo/local_manifests && \
-
-# Initialize repo with specified manifest
-repo init -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs \
-
-# Clone local_manifests repository
-git clone https://github.com/rktdnt/local_manifest --depth 1 -b lineage-21.0 .repo/local_manifests && \
-
-# Sync the repositories
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \ 
-
-# Set up build environment
-source build/envsetup.sh && \
-
-# Lunch configuration
-lunch lineage_tissot-userdebug && \
-
-# Build the ROM
-mka bacon && \
-echo "Date and time:" && \
-
-# Print out/build_date.txt
-cat out/build_date.txt; echo \
-
-# Print SHA256
-sha256sum out/target/product/*/*.zip"
+crave run --clean --no-patch -- "wget -O a.sh https://raw.githubusercontent.com/rktdnt/local_manifest/main/a.sh && chmod +x a.sh && ./a.sh" 
 
 # Clean up
 rm -rf tissot/
@@ -43,5 +17,3 @@ crave pull out/target/product/*/*.zip
 
 # Pull generated img files
 crave pull out/target/product/*/*.img
-
-curl --progress-bar -T "tissot/*.zip" https://pixeldrain.com/api/file/ | cat
