fae18697a9d2ac8868a49899e8a92ccc15a85b4a
Raya Mahardika <rayapkun@gmail.com>
2024-03-17 20:47:53 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,4 +1,4 @@
-rm -rf .repo/local_manifests
+rm -rf *
 # Clone local_manifests repository
 repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android -b 14.0 --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://github.com/rktdnt/local_manifests --depth 1 -b lineage-21 .repo/local_manifests
