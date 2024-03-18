80f16d2e49eb45e400c8485ceed42f997669543e
Raya Mahardika <rayapkun@gmail.com>
2024-03-19 04:30:57 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,9 +1,9 @@
 rm -rf *
 # Clone local_manifests repository
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android -b 14.0 --git-lfs -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorExtended/manifest -b UDC --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://github.com/rktdnt/local_manifests --depth 1 -b lineage-21 .repo/local_manifests
 repo sync -c -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync
 repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
 . build/envsetup.sh
-lunch lineage_tissot-userdebug
-make bacon
+lunch superior_tissot-userdebug
+mka bacon
