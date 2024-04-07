08eb88389895ad1d9644188b6550413a48cc59b2
Raya Mahardika <rayapkun@hlcyn.co>
2024-04-08 04:49:28 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,9 +1,8 @@
-rm -rf *
+rm -rf .repo/local_manifest
 # Clone local_manifests repository
 repo init -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen --git-lfs
 git clone https://github.com/rktdnt/local_manifests --depth 1 -b lance .repo/local_manifests
-repo sync -c -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync
-repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
+/opt/crave/resync.sh
 
 # extras be4 beelding
 rm -rf hardware/mediatek
