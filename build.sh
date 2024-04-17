a3a61be198ede507743894efa2caa875fd05c636
Raya Mahardika <rayapkun@hlcyn.co>
2024-04-17 21:15:44 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 #Credit to Meghthedev for the initial script 
 
 # Initialize repo with specified manifest
-repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
+repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs --depth=1
 
 crave run --no-patch -- "wget -O a.sh https://raw.githubusercontent.com/rktdnt/local_manifest/lineage-21/a.sh && chmod +x a.sh && ./a.sh" 
 
