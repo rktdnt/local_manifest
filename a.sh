b60fdf338c6a9d1e55da4b35af4f18429b92c682
Raya Mahardika <rayapkun@gmail.com>
2024-05-05 13:27:56 +0700

    init

---
+++ a.sh
@@ -0,0 +1,19 @@
+rm -rf .repo/local_manifest
+
+# Clone local_manifests repository
+git clone https://github.com/rktdnt/local_manifests --depth 1 -b citrus .repo/local_manifests
+/opt/crave/resync.sh
+
+# trees
+rm -rf */xiaomi
+git clone --depth=1 https://github.com/cheldump/device_xiaomi_sm6115-common device/xiaomi/sm6115-common
+git clone --depth=1 https://github.com/cheldump/vendor_xiaomi_sm6115-common vendor/xiaomi/sm6115-common
+git clone --depth=1 https://github.com/cheldump/device_xiaomi_citrus device/xiaomi/citrus
+git clone --depth=1 https://github.com/cheldump/vendor_xiaomi_citrus vendor/xiaomi/citrus
+git clone --depth=1 https://github.com/liliumproject/kernel_xiaomi_chime kernel/xiaomi/sm6115
+git clone --depth=1 https://github.com/cheldump/android_hardware_xiaomi hardware/xiaomi
+
+#beelding
+. build/envsetup.sh
+lunch aosp_citrus-ap1a-userdebug
+make bacon
---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+#Credit to Meghthedev for the initial script 
+
+# Initialize repo with specified manifest
+repo init -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen --git-lfs --depth=1
+
+crave run --no-patch -- "wget -O a.sh https://raw.githubusercontent.com/rktdnt/local_manifest/citrus/a.sh && chmod +x a.sh && ./a.sh" 
+
+# Clean up
+rm -rf citrus/
+
+# Pull generated zip files
+crave pull out/target/product/*/*.zip 
+
+# Pull generated img files
+crave pull out/target/product/*/*.img
