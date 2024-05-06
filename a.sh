7ea4c806bdf0f03fb4c4fa0c52a033be636c44d0
Raya Mahardika <rayapkun@hlcyn.co>
2024-05-06 15:16:27 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,15 +1,15 @@
 rm -rf .repo/local_manifest
 
 # Clone local_manifests repository
-git clone https://github.com/rktdnt/local_manifests --depth 1 -b citrus .repo/local_manifests
+git clone https://github.com/rktdnt/local_manifests --depth 1 -b lime .repo/local_manifests
 /opt/crave/resync.sh
 
 # trees
 rm -rf */xiaomi
 git clone --depth=1 https://github.com/cheldump/device_xiaomi_sm6115-common device/xiaomi/sm6115-common
 git clone --depth=1 https://github.com/cheldump/vendor_xiaomi_sm6115-common vendor/xiaomi/sm6115-common
-git clone --depth=1 https://github.com/cheldump/device_xiaomi_citrus device/xiaomi/citrus
-git clone --depth=1 https://github.com/cheldump/vendor_xiaomi_citrus vendor/xiaomi/citrus
+git clone --depth=1 https://github.com/cheldump/device_xiaomi_lime device/xiaomi/lime
+git clone --depth=1 https://github.com/cheldump/vendor_xiaomi_lime vendor/xiaomi/lime
 git clone --depth=1 https://github.com/liliumproject/kernel_xiaomi_chime kernel/xiaomi/sm6115
 git clone --depth=1 https://github.com/cheldump/android_hardware_xiaomi hardware/xiaomi
 
@@ -19,5 +19,5 @@ git clone --depth=1 https://github.com/PixelExperience/packages_resources_device
 
 #beelding
 . build/envsetup.sh
-lunch aosp_citrus-ap1a-userdebug
+lunch aosp_lime-ap1a-userdebug
 make bacon
