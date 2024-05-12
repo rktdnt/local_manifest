45cf5b3962ff2c554b9a006dc65fdc175f6c4d6d
Raya Mahardika <rayapkun@hlcyn.co>
2024-05-12 19:46:49 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,14 +1,11 @@
 rm -rf .repo/local_manifest
 # Clone local_manifests repository
 git clone https://github.com/rktdnt/local_manifests --depth 1 -b lance .repo/local_manifests
-repo init -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen --git-lfs
 /opt/crave/resync.sh
 
 # extras be4 beelding
 rm -rf hardware/mediatek
-rm -rf device/mediatek/sepolicy_vndr
 git clone https://github.com/LineageOS/android_hardware_mediatek hardware/mediatek -b lineage-21
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr -b lineage-21
 
 # patches
 rm -rf frameworks/av
@@ -16,11 +13,9 @@ git clone --depth=1 https://github.com/cheldump/frameworks_av frameworks/av
 
 # trees
 rm -rf */xiaomi
-git clone --depth=1 https://github.com/cheldump/android_device_xiaomi_lancelot device/xiaomi/lancelot
-git clone --depth=1 https://github.com/cheldump/proprietary_vendor_xiaomi_lancelot vendor/xiaomi/lancelot
-git clone --depth=1 https://github.com/cheldump/android_device_xiaomi_mt6768-common device/xiaomi/mt6768-common
-git clone --depth=1 https://github.com/cheldump/proprietary_vendor_xiaomi_mt6768-common vendor/xiaomi/mt6768-common
-git clone --depth=1 https://gitlab.com/MT6768Lab/KernelTree kernel/xiaomi/mt6768
+git clone --depth=1 https://github.com/cheldump/device_xiaomi_lancelot device/xiaomi/lancelot
+git clone --depth=1 https://github.com/cheldump/vendor_xiaomi_lancelot vendor/xiaomi/lancelot
+git clone --depth=1 https://gitlab.com/MT6768Lab/KernelTree kernel/xiaomi/mt6768 -b 14
 git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
 
 #beelding
