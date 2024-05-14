151061ac31f6816299fbc0dd35881a8ea4ef1a9e
Raya Mahardika <rayapkun@hlcyn.co>
2024-05-15 06:33:17 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -6,16 +6,16 @@ git clone https://github.com/rktdnt/local_manifests --depth 1 -b citrus .repo/lo
 
 # trees
 rm -rf */xiaomi
-git clone --depth=1 https://github.com/cheldump/device_xiaomi_sm6115-common device/xiaomi/sm6115-common
-git clone --depth=1 https://github.com/cheldump/vendor_xiaomi_sm6115-common vendor/xiaomi/sm6115-common
+git clone --depth=1 https://github.com/cheldump/device_xiaomi_sm6115-common device/xiaomi/sm6115-common -b fourteen
+git clone --depth=1 https://github.com/cheldump/vendor_xiaomi_sm6115-common vendor/xiaomi/sm6115-common -b fourteen
 git clone --depth=1 https://github.com/cheldump/device_xiaomi_citrus device/xiaomi/citrus
 git clone --depth=1 https://github.com/cheldump/vendor_xiaomi_citrus vendor/xiaomi/citrus
 git clone --depth=1 https://github.com/liliumproject/kernel_xiaomi_chime kernel/xiaomi/sm6115
 git clone --depth=1 https://github.com/cheldump/android_hardware_xiaomi hardware/xiaomi
 
 #lu kanjut
-rm -rf packages/resources/devicesettings
-git clone --depth=1 https://github.com/PixelExperience/packages_resources_devicesettings packages/resources/devicesettings
+#rm -rf packages/resources/devicesettings
+#git clone --depth=1 https://github.com/PixelExperience/packages_resources_devicesettings packages/resources/devicesettings
 
 #beelding
 . build/envsetup.sh
