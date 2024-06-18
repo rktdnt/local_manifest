1f254b6f7721e57177f9d2b9172146aac2d75f78
Raya Mahardika <rayapkun@hlcyn.co>
2024-06-18 14:06:33 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,5 +1,6 @@
 rm -rf .repo/local_manifest
 # Clone local_manifests repository
+repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
 /opt/crave/resync.sh
 
 # trees
@@ -10,8 +11,9 @@ git clone --depth=1 https://github.com/cheldump/android_device_xiaomi_tissot dev
 git clone --depth=1 https://github.com/cheldump/proprietary_vendor_xiaomi_tissot vendor/xiaomi/tissot
 git clone --depth=1 https://github.com/xiaomi-msm8953-devs/android_kernel_xiaomi_msm8953 kernel/xiaomi/msm8953
 git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
+git clone --depth=1 https://github.com/rktdnt/p3 vendor/lineage-priv/keys
 
 #beelding
 . build/envsetup.sh
-lunch lineage_tissot-ap1a-userdebug
-make bacon
+riseup tissot userdebug
+rise b
