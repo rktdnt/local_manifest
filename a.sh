8d1c043e678889454a7be990e796e0e6a8c65e26
Raya Mahardika <rayapkun@hlcyn.co>
2024-06-30 01:52:45 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,17 +1,16 @@
 # sync
-repo init -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen --git-lfs --depth=1
+repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
 /opt/crave/resync.sh
 
-rm -rf vendor/aosp/signing/keys
-git clone https://github.com/rktdnt/p3 -b pos vendor/aosp/signing/keys
+git clone --depth=1 https://github.com/rktdnt/p3 vendor/lineage-priv/keys
 
 # extras be4 beelding
 rm -rf hardware/mediatek
 git clone https://github.com/LineageOS/android_hardware_mediatek hardware/mediatek -b lineage-21
 
 # patches
-rm -rf frameworks/av
-git clone --depth=1 https://github.com/cheldump/frameworks_av frameworks/av
+#rm -rf frameworks/av
+#git clone --depth=1 https://github.com/cheldump/frameworks_av frameworks/av
 
 # trees
 rm -rf */xiaomi
@@ -21,6 +20,7 @@ git clone --depth=1 https://gitlab.com/MT6768Lab/KernelTree kernel/xiaomi/lancel
 git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
 
 #beelding
+#beelding
 . build/envsetup.sh
-lunch aosp_lancelot-ap2a-userdebug
-make installclean && mka bacon
+riseup lancelot userdebug
+rise b
