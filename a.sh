882449a6d79c39fbde9cda5980e8cdc77d83143e
Raya Mahardika <rayapkun@hlcyn.co>
2024-07-05 21:35:27 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -6,11 +6,11 @@ rm -rf .repo/local_manifest
 # trees
 rm -rf vendor/lineage-priv/keys
 rm -rf */xiaomi
-git clone --depth=1 https://github.com/cheldump/android_device_xiaomi_msm8953-common device/xiaomi/msm8953-common
-git clone --depth=1 https://github.com/cheldump/proprietary_vendor_xiaomi_msm8953-common vendor/xiaomi/msm8953-common
-git clone --depth=1 https://github.com/cheldump/android_device_xiaomi_tissot device/xiaomi/tissot
-git clone --depth=1 https://github.com/cheldump/proprietary_vendor_xiaomi_tissot vendor/xiaomi/tissot
-git clone --depth=1 https://github.com/xiaomi-msm8953-devs/android_kernel_xiaomi_msm8953 kernel/xiaomi/msm8953
+#git clone --depth=1 https://github.com/cheldump/android_device_xiaomi_msm8953-common device/xiaomi/msm8953-common
+#git clone --depth=1 https://github.com/cheldump/proprietary_vendor_xiaomi_msm8953-common vendor/xiaomi/msm8953-common
+git clone --depth=1 https://github.com/cheldump/android_device_xiaomi_tissot device/xiaomi/tissot -b dec
+git clone --depth=1 https://github.com/cheldump/proprietary_vendor_xiaomi_tissot vendor/xiaomi/tissot -b dec
+git clone --depth=1 https://github.com/xiaomi-msm8953-devs/android_kernel_xiaomi_msm8953 kernel/xiaomi/tissot
 git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
 git clone --depth=1 https://github.com/rktdnt/p3 vendor/lineage-priv/keys
 
