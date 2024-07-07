04d12af5664e33ec0bfe0178522a356e01544a6c
Raya Mahardika <rayapkun@hlcyn.co>
2024-07-07 09:45:45 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,20 +1,20 @@
-rm -rf .repo/local_manifest
-# Clone local_manifests repository
 #repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
 /opt/crave/resync.sh
 
+# signing
+rm -rf vendor/derp/signing/keys
+git clone https://github.com/rktdnt/p3 -b pos vendor/derp/signing/keys
+
 # trees
-rm -rf vendor/lineage-priv/keys
 rm -rf */xiaomi
-#git clone --depth=1 https://github.com/cheldump/android_device_xiaomi_msm8953-common device/xiaomi/msm8953-common
-#git clone --depth=1 https://github.com/cheldump/proprietary_vendor_xiaomi_msm8953-common vendor/xiaomi/msm8953-common
-git clone --depth=1 https://github.com/cheldump/android_device_xiaomi_tissot device/xiaomi/tissot -b dec
-git clone --depth=1 https://github.com/cheldump/proprietary_vendor_xiaomi_tissot vendor/xiaomi/tissot -b dec
-git clone --depth=1 https://github.com/xiaomi-msm8953-devs/android_kernel_xiaomi_msm8953 kernel/xiaomi/tissot
+git clone --depth=1 https://github.com/cheldump/android_device_xiaomi_msm8953-common device/xiaomi/msm8953-common
+git clone --depth=1 https://github.com/cheldump/proprietary_vendor_xiaomi_msm8953-common vendor/xiaomi/msm8953-common
+git clone --depth=1 https://github.com/cheldump/android_device_xiaomi_tissot device/xiaomi/tissot
+git clone --depth=1 https://github.com/cheldump/proprietary_vendor_xiaomi_tissot vendor/xiaomi/tissot
+git clone --depth=1 https://github.com/xiaomi-msm8953-devs/android_kernel_xiaomi_msm8953 kernel/xiaomi/msm8953
 git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
-git clone --depth=1 https://github.com/rktdnt/p3 vendor/lineage-priv/keys
 
 #beelding
 . build/envsetup.sh
-lunch lineage_tissot-ap2a-userdebug
-mka bacon
+lunch derp_tissot-user
+mka derp
