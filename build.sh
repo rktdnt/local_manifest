1217c80d285a863144398633a048743e47083571
Raya Mahardika <rayapkun@hlcyn.co>
2024-07-07 10:00:55 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 #Credit to Meghthedev for the initial script 
 
 # Initialize repo with specified manifest
-repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 14 --git-lfs --depth=1
+repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs --depth=1
 crave run --no-patch -- "wget -O a.sh https://raw.githubusercontent.com/rktdnt/local_manifest/tissot/a.sh && chmod +x a.sh && ./a.sh" 
 
 # Clean up
