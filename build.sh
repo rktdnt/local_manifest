860740b108b35d069b59bb94347d8bd73ef79503
Raya Mahardika <rayapkun@hlcyn.co>
2024-07-23 17:39:09 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ set -e
 #Credit to Meghthedev for the initial script 
 
 # Initialize repo with specified manifest
-repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs --depth=1
-crave set --projectID 72
+repo init -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen --git-lfs
+crave set --projectID 82
 
 crave run --no-patch -- "wget -O a.sh https://raw.githubusercontent.com/rktdnt/local_manifest/lance/a.sh && chmod +x a.sh && ./a.sh" 
 
