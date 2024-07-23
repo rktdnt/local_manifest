2aaed924cfdda022034409c0cc77804802129ebc
Raya Mahardika <rayapkun@hlcyn.co>
2024-07-23 17:47:02 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,8 +1,8 @@
 # sync
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
 /opt/crave/resync.sh
 
-git clone --depth=1 https://github.com/rktdnt/p3 vendor/lineage-priv/keys
+rm -rf vendor/aosp/signing/keys
+git clone https://github.com/rktdnt/p3 -b pos vendor/aosp/signing/keys
 
 # extras be4 beelding
 rm -rf hardware/mediatek
@@ -15,12 +15,11 @@ git clone https://github.com/LineageOS/android_hardware_mediatek hardware/mediat
 # trees
 rm -rf */xiaomi
 git clone --depth=1 https://github.com/cheldump/device_xiaomi_lancelot device/xiaomi/lancelot
-git clone --depth=1 https://github.com/cheldump/vendor_xiaomi_lancelot vendor/xiaomi/lancelot -b abc
+git clone --depth=1 https://github.com/cheldump/vendor_xiaomi_lancelot vendor/xiaomi/lancelot
 git clone --depth=1 https://gitlab.com/MT6768Lab/KernelTree kernel/xiaomi/lancelot -b 14
 git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
 
 #beelding
-#beelding
 . build/envsetup.sh
-riseup lancelot userdebug
-rise b
+lunch aosp_lancelot-ap2a-userdebug
+make installclean && mka bacon
