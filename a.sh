bed3888e75c539eff47357499c213c2523d16b95
Raya Mahardika <rayapkun@hlcyn.co>
2024-08-02 21:43:52 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,14 +1,13 @@
 #
+repo init -u https://github.com/ProjectEverest/manifest -b qpr3 --git-lfs
 /opt/crave/resync.sh
 
-rm -rf out
-
 # signing
-rm -rf vendor/lineage-priv/keys
-git clone --depth=1 https://github.com/rktdnt/p3 vendor/lineage-priv/keys
+#rm -rf vendor/lineage-priv/keys
+#git clone --depth=1 https://github.com/rktdnt/p3 vendor/lineage-priv/keys
 
-#rm -rf vendor/derp/signing/keys
-#git clone https://github.com/rktdnt/p3 -b pos vendor/derp/signing/keys
+rm -rf vendor/everest/signing/keys
+git clone https://github.com/rktdnt/p3 -b pos vendor/everest/signing/keys
 
 # trees
 rm -rf */xiaomi
@@ -16,10 +15,10 @@ git clone --depth=1 https://github.com/cheldump/android_device_xiaomi_msm8953-co
 git clone --depth=1 https://github.com/cheldump/proprietary_vendor_xiaomi_msm8953-common vendor/xiaomi/msm8953-common
 git clone --depth=1 https://github.com/cheldump/android_device_xiaomi_tissot device/xiaomi/tissot
 git clone --depth=1 https://github.com/cheldump/proprietary_vendor_xiaomi_tissot vendor/xiaomi/tissot
-git clone --depth=1 https://github.com/cheldump/android_kernel_xiaomi_msm8953 kernel/xiaomi/msm8953
+git clone --depth=1 https://github.com/xiaomi-msm8953-devs/android_kernel_xiaomi_msm8953 kernel/xiaomi/msm8953
 git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
 
 #beelding
 . build/envsetup.sh
-riseup tissot userdebug
-rise b
+peak tissot user
+climb b
