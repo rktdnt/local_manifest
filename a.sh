b182a09aa0fd44080cade3417e3947eabc917689
Raya Mahardika <rayapkun@hlcyn.co>
2024-08-26 21:32:39 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -10,11 +10,11 @@ git clone --depth=1 https://github.com/rktdnt/p3 vendor/lineage-priv/keys
 
 # trees
 rm -rf */xiaomi
-git clone --depth=1 https://github.com/cheldump/android_device_xiaomi_msm8953-common device/xiaomi/msm8953-common
+git clone --depth=1 https://github.com/cheldump/android_device_xiaomi_msm8953-common -b lineg device/xiaomi/msm8953-common
 git clone --depth=1 https://github.com/cheldump/proprietary_vendor_xiaomi_msm8953-common vendor/xiaomi/msm8953-common
-git clone --depth=1 https://github.com/cheldump/android_device_xiaomi_tissot device/xiaomi/tissot
+git clone --depth=1 https://github.com/cheldump/android_device_xiaomi_tissot -b lineg device/xiaomi/tissot
 git clone --depth=1 https://github.com/cheldump/proprietary_vendor_xiaomi_tissot vendor/xiaomi/tissot
-git clone --depth=1 https://github.com/xiaomi-msm8953-devs/android_kernel_xiaomi_msm8953 kernel/xiaomi/msm8953
+git clone --depth=1 https://github.com/cheldump/android_kernel_xiaomi_msm8953 kernel/xiaomi/msm8953
 git clone --depth=1 https://github.com/PixelOS-AOSP/hardware_xiaomi hardware/xiaomi
 
 #beelding
