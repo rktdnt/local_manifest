d85613a5a37639a0b2626e71424ca2f376b086dc
Raya Mahardika <rayapkun@hlcyn.co>
2024-09-01 08:22:33 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ set -e
 #Credit to Meghthedev for the initial script 
 
 # Initialize repo with specified manifest
-repo init -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen --git-lfs
-crave set --projectID 82
+repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
+crave set --projectID 86
 
 crave run --no-patch -- "wget -O a.sh https://raw.githubusercontent.com/rktdnt/local_manifest/lance/a.sh && chmod +x a.sh && ./a.sh" 
 
@@ -17,10 +17,3 @@ crave pull out/target/product/*/*.zip
 
 # Pull generated img files
 crave pull out/target/product/*/*.img
-
-# upload
-git clone https://github.com/rktdnt/releases
-rm -rf *ota*.zip
-mv *.zip releases/
-cd releases
-bash release.sh
