1d03cfb73aa115fa21741cda48dfc2856db2954b
Raya Mahardika <rayapkun@hlcyn.co>
2024-09-03 22:18:33 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -2,24 +2,24 @@
 /opt/crave/resync.sh
 
 # signing
-rm -rf vendor/lineage-priv/keys
-git clone --depth=1 https://github.com/rktdnt/p3 vendor/lineage-priv/keys
+#rm -rf vendor/lineage-priv/keys
+#git clone --depth=1 https://github.com/rktdnt/p3 vendor/lineage-priv/keys
 
-#rm -rf vendor/everest/signing/keys
-#git clone https://github.com/rktdnt/p3 -b pos vendor/everest/signing/keys
+rm -rf vendor/derp/signing/keys
+git clone https://github.com/rktdnt/p3 -b pos vendor/everest/signing/keys
 
 # trees
 rm -rf */xiaomi
-git clone --depth=1 https://github.com/cheldump/android_device_xiaomi_msm8953-common device/xiaomi/msm8953-common
-git clone --depth=1 https://github.com/cheldump/proprietary_vendor_xiaomi_msm8953-common vendor/xiaomi/msm8953-common
-git clone --depth=1 https://github.com/cheldump/android_device_xiaomi_tissot device/xiaomi/tissot
-git clone --depth=1 https://github.com/cheldump/proprietary_vendor_xiaomi_tissot vendor/xiaomi/tissot
-git clone --depth=1 https://github.com/cheldump/android_kernel_xiaomi_msm8953 kernel/xiaomi/msm8953
-git clone --depth=1 https://github.com/PixelOS-AOSP/hardware_xiaomi hardware/xiaomi
+git clone --depth=1 https://github.com/plrine/android_device_xiaomi_msm8953-common device/xiaomi/msm8953-common
+git clone --depth=1 https://github.com/plrine/proprietary_vendor_xiaomi_msm8953-common vendor/xiaomi/msm8953-common
+git clone --depth=1 https://github.com/plrine/android_device_xiaomi_tissot device/xiaomi/tissot
+git clone --depth=1 https://github.com/plrine/proprietary_vendor_xiaomi_tissot vendor/xiaomi/tissot
+git clone --depth=1 https://github.com/plrine/android_kernel_xiaomi_msm8953 kernel/xiaomi/msm8953
+git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
 
 rm -rf prebuilts/clang/host/linux-x86/clang-r510928
 
 #beelding
 . build/envsetup.sh
-riseup tissot user
-rise b
+lunch derp_tissot-user
+mka derp -j$(nproc --all)
