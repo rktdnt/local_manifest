4bce9896094e6af96c696771c9c731465ea0966f
Raya Mahardika <rayapkun@hlcyn.co>
2024-09-03 22:24:51 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ set -e
 #Credit to Meghthedev for the initial script 
 # Initialize repo with specified manifest
 repo init -u https://github.com/DerpFest-AOSP/manifest -b 14
+crave set --projectID 64
 
 crave run --no-patch -- "wget -O a.sh https://raw.githubusercontent.com/rktdnt/local_manifest/tissot/a.sh && chmod +x a.sh && ./a.sh" 
-crave set --projectID 64
 
 # Clean up
 rm -rf tissot/
