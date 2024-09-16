4a577d144ce32b4f38b34c6323ab6e2401918a6f
Raya Mahardika <rayapkun@hlcyn.co>
2024-09-16 08:42:03 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,23 +1,24 @@
 #
+repo init -u https://github.com/StyxProject/manifest -b vic
 /opt/crave/resync.sh
 
 # signing
 #rm -rf vendor/lineage-priv/keys
 #git clone --depth=1 https://github.com/rktdnt/p3 vendor/lineage-priv/keys
 
-rm -rf vendor/derp/signing/keys
-git clone https://github.com/rktdnt/p3 -b pos vendor/derp/signing/keys
+#rm -rf vendor/derp/signing/keys
+#git clone https://github.com/rktdnt/p3 -b pos vendor/derp/signing/keys
 
 # trees
 rm -rf */xiaomi
-git clone --depth=1 https://github.com/plrine/android_device_xiaomi_msm8953-common -b derp device/xiaomi/msm8953-common
+git clone --depth=1 https://github.com/plrine/android_device_xiaomi_msm8953-common device/xiaomi/msm8953-common
 git clone --depth=1 https://github.com/plrine/proprietary_vendor_xiaomi_msm8953-common vendor/xiaomi/msm8953-common
-git clone --depth=1 https://github.com/plrine/android_device_xiaomi_tissot -b derp device/xiaomi/tissot
+git clone --depth=1 https://github.com/plrine/android_device_xiaomi_tissot device/xiaomi/tissot
 git clone --depth=1 https://github.com/plrine/proprietary_vendor_xiaomi_tissot vendor/xiaomi/tissot
 git clone --depth=1 https://github.com/plrine/android_kernel_xiaomi_msm8953 kernel/xiaomi/msm8953
 git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
 
 #beelding
 . build/envsetup.sh
-lunch derp_tissot-user
-mka derp -j$(nproc --all)
+lunch tissot-ap3a-user
+m styx-ota -j$(nproc --all)
