551dd7a751d14a51e61c3df80d97bcc061106fd8
Raya Mahardika <rayapkun@hlcyn.co>
2024-09-19 01:12:17 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 set -e
 #Credit to Meghthedev for the initial script 
 # Initialize repo with specified manifest
-repo init -u https://android.googlesource.com/platform/manifest -b android-15.0.0_r1
-crave set --projectID 35
+repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
+#crave set --projectID 35
 
 crave run --no-patch -- "wget -O a.sh https://raw.githubusercontent.com/rktdnt/local_manifest/tissot/a.sh && chmod +x a.sh && ./a.sh" 
 
