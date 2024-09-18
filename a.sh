d1ffa1e20ec98c68a774668b9e5f430c25dbae97
Raya Mahardika <rayapkun@hlcyn.co>
2024-09-19 01:11:15 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,10 +1,9 @@
 #
-repo init -u https://github.com/StyxProject/manifest -b vic
 /opt/crave/resync.sh
 
 # signing
-#rm -rf vendor/lineage-priv/keys
-#git clone --depth=1 https://github.com/rktdnt/p3 vendor/lineage-priv/keys
+rm -rf vendor/lineage-priv/keys
+git clone --depth=1 https://github.com/rktdnt/p3 vendor/lineage-priv/keys
 
 #rm -rf vendor/derp/signing/keys
 #git clone https://github.com/rktdnt/p3 -b pos vendor/derp/signing/keys
@@ -16,14 +15,14 @@ git clone --depth=1 https://github.com/plrine/proprietary_vendor_xiaomi_msm8953-
 git clone --depth=1 https://github.com/plrine/android_device_xiaomi_tissot device/xiaomi/tissot
 git clone --depth=1 https://github.com/plrine/proprietary_vendor_xiaomi_tissot vendor/xiaomi/tissot
 git clone --depth=1 https://github.com/plrine/android_kernel_xiaomi_msm8953 kernel/xiaomi/msm8953
-git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
+git clone --depth=1 https://github.com/plrine/hardware_xiaomi hardware/xiaomi
 
 # hals
-git clone --depth=1 https://github.com/LineageOS/android_hardware_qcom_media -b lineage-21.0-caf-msm8996 hardware/qcom-caf/msm8996/media
-git clone --depth=1 https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-21.0-caf-msm8996 hardware/qcom-caf/msm8996/audio
-git clone --depth=1 https://github.com/LineageOS/android_hardware_qcom_display -b lineage-21.0-caf-msm8996 hardware/qcom-caf/msm8996/display
+#git clone --depth=1 https://github.com/LineageOS/android_hardware_qcom_media -b lineage-21.0-caf-msm8996 hardware/qcom-caf/msm8996/media
+#git clone --depth=1 https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-21.0-caf-msm8996 hardware/qcom-caf/msm8996/audio
+#git clone --depth=1 https://github.com/LineageOS/android_hardware_qcom_display -b lineage-21.0-caf-msm8996 hardware/qcom-caf/msm8996/display
 
 #beelding
 . build/envsetup.sh
-lunch tissot-ap3a-user
-m styx-ota -j$(nproc --all)
+lunch lineage_tissot-ap2a-user
+make bacon -j$(nproc --all)
