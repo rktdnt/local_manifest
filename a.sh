830b2607fcb9f0d9cbc1bb53bbf38821950f94c4
Raya Mahardika <rayapkun@hlcyn.co>
2024-10-12 17:24:25 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,10 +1,10 @@
 #
-repo init -u https://github.com/LineageOS/android.git -b lineage-22.0 --git-lfs
+repo init -u https://github.com/AospEnhanced/android_manifest -b fifteen
 /opt/crave/resync.sh
 
 # signing
-rm -rf vendor/lineage-priv/keys
-git clone --depth=1 https://github.com/rktdnt/p3 vendor/lineage-priv/keys
+#rm -rf vendor/lineage-priv/keys
+#git clone --depth=1 https://github.com/rktdnt/p3 vendor/lineage-priv/keys
 
 #rm -rf vendor/derp/signing/keys
 #git clone https://github.com/rktdnt/p3 -b pos vendor/derp/signing/keys
@@ -16,7 +16,7 @@ git clone --depth=1 https://github.com/plrine/proprietary_vendor_xiaomi_msm8953-
 git clone --depth=1 https://github.com/plrine/android_device_xiaomi_tissot device/xiaomi/tissot
 git clone --depth=1 https://github.com/plrine/proprietary_vendor_xiaomi_tissot vendor/xiaomi/tissot
 git clone --depth=1 https://github.com/plrine/android_kernel_xiaomi_msm8953 kernel/xiaomi/msm8953
-git clone --depth=1 https://github.com/plrine/hardware_xiaomi hardware/xiaomi
+git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
 
 # hals
 #git clone --depth=1 https://github.com/LineageOS/android_hardware_qcom_media -b lineage-21.0-caf-msm8996 hardware/qcom-caf/msm8996/media
@@ -25,5 +25,5 @@ git clone --depth=1 https://github.com/plrine/hardware_xiaomi hardware/xiaomi
 
 #beelding
 . build/envsetup.sh
-lunch lineage_tissot-ap3a-user
+lunch aosp_tissot-ap3a-user
 make bacon -j$(nproc --all)
