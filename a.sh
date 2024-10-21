a7748b10828d0b30886e705e1dda98e1f0731c31
Raya Mahardika <rayapkun@hlcyn.co>
2024-10-21 22:08:02 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -11,12 +11,14 @@ git clone --depth=1 https://github.com/rktdnt/p3 vendor/lineage-priv/keys
 # extras be4 beelding
 rm -rf hardware/mediatek
 git clone https://github.com/LineageOS/android_hardware_mediatek hardware/mediatek -b lineage-21
+rm -rf device/mediatek/sepolicy_vndr
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr -b lineage-21 
 
 # trees
 rm -rf */xiaomi
 git clone --depth=1 https://github.com/cheldump/device_xiaomi_lancelot device/xiaomi/lancelot
 git clone --depth=1 https://github.com/cheldump/vendor_xiaomi_lancelot vendor/xiaomi/lancelot
-git clone --depth=1 https://gitlab.com/MT6768Lab/KernelTree kernel/xiaomi/lancelot -b 14
+git clone --depth=1 https://github.com/cheldump/kernel_xiaomi_lancelot kernel/xiaomi/lancelot -b 14
 git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
 
 #beelding
