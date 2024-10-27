ce51cafcc226a8391ae1a5aec6d18d35781f7a17
Raya Mahardika <rayapkun@hlcyn.co>
2024-10-27 20:38:34 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@
 set -e
 #Credit to Meghthedev for the initial script 
 # Initialize repo with specified manifest
-repo init -u https://android.googlesource.com/platform/manifest -b android-15.0.0_r1
-crave set --projectID 35
+repo init -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen --git-lfs
 
 crave run --no-patch -- "wget -O a.sh https://raw.githubusercontent.com/rktdnt/local_manifest/tissot/a.sh && chmod +x a.sh && ./a.sh" 
 
