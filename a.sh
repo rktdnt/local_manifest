644bbd317be265bd7fa25b54ef07350a808e3d3e
Raya Mahardika <rayapkun@hlcyn.co>
2024-11-18 08:50:45 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -5,12 +5,12 @@
 #rm -rf vendor/lineage-priv/keys
 #git clone --depth=1 https://github.com/rktdnt/p3 vendor/lineage-priv/keys
 
-rm -rf vendor/aosp/signing/keys
-git clone https://github.com/rktdnt/p3 -b pos vendor/aosp/signing/keys
+#rm -rf vendor/aosp/signing/keys
+#git clone https://github.com/rktdnt/p3 -b pos vendor/aosp/signing/keys
 
 # fp
-rm -rf vendor/extra
-git clone https://github.com/plrine/fp vendor/extra
+#rm -rf vendor/extra
+#git clone https://github.com/plrine/fp vendor/extra
 
 # trees
 rm -rf */xiaomi
@@ -19,11 +19,11 @@ git clone --depth=1 https://github.com/plrine/proprietary_vendor_xiaomi_msm8953-
 git clone --depth=1 https://github.com/plrine/android_device_xiaomi_tissot device/xiaomi/tissot
 git clone --depth=1 https://github.com/plrine/proprietary_vendor_xiaomi_tissot vendor/xiaomi/tissot
 git clone --depth=1 https://github.com/plrine/android_kernel_xiaomi_msm8953 kernel/xiaomi/msm8953
-git clone --depth=1 https://github.com/PixelOS-AOSP/hardware_xiaomi hardware/xiaomi
+git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
 
-cd vendor/aosp
-git fetch https://github.com/pos-gm/vendor_aosp && git cherry-pick 2a8e8eeb913c27618f903a88d243fa268b6a4cbe
-cd ../..
+#cd vendor/aosp
+#git fetch https://github.com/pos-gm/vendor_aosp && git cherry-pick 2a8e8eeb913c27618f903a88d243fa268b6a4cbe
+#cd ../..
 
 # hals
 #git clone --depth=1 https://github.com/LineageOS/android_hardware_qcom_media -b lineage-21.0-caf-msm8996 hardware/qcom-caf/msm8996/media
@@ -32,5 +32,5 @@ cd ../..
 
 #beelding
 . build/envsetup.sh
-lunch aosp_tissot-ap2a-user
+lunch lineage_tissot-ap2a-user
 mka bacon -j$(nproc --all)
