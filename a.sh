08f2a2e196e73ab2499ffe641e382c4656f5dbc9
Raya Mahardika <rayapkun@hlcyn.co>
2024-11-27 18:06:29 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -19,7 +19,7 @@ git clone --depth=1 https://github.com/plrine/proprietary_vendor_xiaomi_msm8953-
 git clone --depth=1 https://github.com/plrine/android_device_xiaomi_tissot device/xiaomi/tissot
 git clone --depth=1 https://github.com/plrine/proprietary_vendor_xiaomi_tissot vendor/xiaomi/tissot
 git clone --depth=1 https://github.com/plrine/android_kernel_xiaomi_msm8953 kernel/xiaomi/msm8953
-git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
+git clone --depth=1 https://github.com/plrine/hardware_xiaomi hardware/xiaomi
 
 #cd vendor/aosp
 #git fetch https://github.com/pos-gm/vendor_aosp && git cherry-pick 2a8e8eeb913c27618f903a88d243fa268b6a4cbe
