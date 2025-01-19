1359c977e189c06b36ded6af917d1eb4a178a63d
Raya Mahardika <rayapkun@hlcyn.co>
2025-01-19 07:35:28 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,13 +1,12 @@
 #
-repo init -u https://github.com/LineageOS/android.git -b lineage-22.1 --git-lfs
 /opt/crave/resync.sh
 
 # signing
 #rm -rf vendor/lineage-priv/keys
 #git clone --depth=1 https://github.com/rktdnt/p3 vendor/lineage-priv/keys
 
-#rm -rf vendor/aosp/signing/keys
-#git clone https://github.com/rktdnt/p3 -b pos vendor/aosp/signing/keys
+rm -rf vendor/derp/signing/keys
+git clone https://github.com/rktdnt/p3 -b pos vendor/derp/signing/keys
 
 # wlan
 #rm -rf hardware/qcom-caf/wlan
@@ -15,13 +14,13 @@ repo init -u https://github.com/LineageOS/android.git -b lineage-22.1 --git-lfs
 
 # trees
 rm -rf */xiaomi
-git clone --depth=1 https://github.com/plrine/android_device_xiaomi_msm8953-common device/xiaomi/msm8953-common
-git clone --depth=1 https://github.com/plrine/proprietary_vendor_xiaomi_msm8953-common vendor/xiaomi/msm8953-common
-git clone --depth=1 https://github.com/plrine/android_device_xiaomi_tissot device/xiaomi/tissot
-git clone --depth=1 https://github.com/plrine/proprietary_vendor_xiaomi_tissot vendor/xiaomi/tissot
+git clone --depth=1 https://github.com/plrine/android_device_xiaomi_msm8953-common device/xiaomi/msm8953-common -b lineage-21
+git clone --depth=1 https://github.com/plrine/proprietary_vendor_xiaomi_msm8953-common vendor/xiaomi/msm8953-common -b lineage-21
+git clone --depth=1 https://github.com/plrine/android_device_xiaomi_tissot device/xiaomi/tissot -b lineage-21
+git clone --depth=1 https://github.com/plrine/proprietary_vendor_xiaomi_tissot vendor/xiaomi/tissot -b lineage-21
 git clone --depth=1 https://github.com/plrine/android_kernel_xiaomi_msm8953 kernel/xiaomi/msm8953
 git clone --depth=1 https://github.com/plrine/hardware_xiaomi hardware/xiaomi
-git clone --depth=1 https://github.com/LineageOS/android_hardware_sony_timekeep hardware/sony/timekeep
+#git clone --depth=1 https://github.com/LineageOS/android_hardware_sony_timekeep hardware/sony/timekeep
 
 # patches
 #cd vendor/aosp
@@ -29,12 +28,12 @@ git clone --depth=1 https://github.com/LineageOS/android_hardware_sony_timekeep 
 #cd ../..
 
 # hals
-rm -rf hardware/qcom-caf/msm8996/display
+#rm -rf hardware/qcom-caf/msm8996/display
 #git clone --depth=1 https://github.com/LineageOS/android_hardware_qcom_media -b lineage-22.1-caf-msm8996 hardware/qcom-caf/msm8996/media
 #git clone --depth=1 https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-22.1-caf-msm8996 hardware/qcom-caf/msm8996/audio
-git clone --depth=1 https://github.com/plrine/android_hardware_qcom_display -b lineage-22.1-caf-msm8996 hardware/qcom-caf/msm8996/display
+#git clone --depth=1 https://github.com/plrine/android_hardware_qcom_display -b lineage-22.1-caf-msm8996 hardware/qcom-caf/msm8996/display
 
 #beelding
 . build/envsetup.sh
-lunch lineage_tissot-ap3a-userdebug
-mka bacon -j$(nproc --all)
+lunch derp_tissot-user
+mka derp -j$(nproc --all)
