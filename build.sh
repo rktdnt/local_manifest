1383cdf94b5899aba3142e674fe99778183275cb
Raya Mahardika <rayapkun@hlcyn.co>
2025-01-19 07:36:19 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 set -e
 #Credit to Meghthedev for the initial script 
 # Initialize repo with specified manifest
-repo init -u https://github.com/accupara/los22.git -b lineage-22.0 --git-lfs
-crave set --projectID 93
+repo init -u https://github.com/DerpFest-AOSP/manifest -b 14 --git-lfs
+crave set --projectID 64
 
 crave run --no-patch -- "curl -fsSL https://raw.githubusercontent.com/rktdnt/local_manifest/tissot/a.sh | bash" 
 
