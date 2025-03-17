82dc013f935f39bd73ae7e398cd705e6b93e774a
Raya Mahardika <rayapkun@hlcyn.co>
2025-03-17 22:45:12 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -14,6 +14,9 @@ git clone --depth=1 https://github.com/plrine/android_device_realme_RM6785-commo
 git clone --depth=1 https://github.com/plrine/device_realme_RMX2001L1 device/realme/RMX2001L1
 git clone --depth=1 https://github.com/plrine/proprietary_vendor_realme_RM6785-common vendor/realme/RM6785-common
 
+git clone --depth=1 https://github.com/halcyonproject/hardware_mediatek hardware/mediatek -b 15.1
+git clone --depth=1 https://github.com/halcyonproject/device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr -b 15.1
+
 #beelding
 . build/envsetup.sh
 lunch halcyon_tissot-ap4a-userdebug
