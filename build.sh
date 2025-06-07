fea402fbb403d739a7d75007554f04bb7d83d263
Raya Mahardika <rayapkun@hlcyn.co>
2025-06-08 00:52:59 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 set -e
 #Credit to Meghthedev for the initial script 
 # Initialize repo with specified manifest
-repo init -u https://android.googlesource.com/platform/manifest -b android-15.0.0_r17
-crave set --projectID 35
+repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
+crave set --projectID 72
 
-crave run --no-patch -- "curl -fsSL https://raw.githubusercontent.com/rktdnt/local_manifest/rmx2001/a.sh | bash" 
+crave run --no-patch -- "curl -fsSL https://raw.githubusercontent.com/rktdnt/local_manifest/pond/a.sh | bash" 
 
 # Clean up
 rm -rf tissot/
