c458ef823def2eb1dd7e21090e00f6beae309a6c
Raya Mahardika <rayapkun@hlcyn.co>
2025-06-08 00:50:33 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,23 +1,22 @@
 #
-repo init -u https://github.com/halcyonproject/manifest -b 15.1 --git-lfs
 /opt/crave/resync.sh
 
 # signing
 #rm -rf vendor/lineage-priv/keys
 #git clone --depth=1 https://github.com/rktdnt/p3 vendor/lineage-priv/keys
 
-git clone https://github.com/rktdnt/p3 -b 15.1 vendor/extra
+#git clone https://github.com/rktdnt/p3 -b 15.1 vendor/extra
 
 # trees
-rm -rf */realme
-git clone --depth=1 https://github.com/plrine/android_device_realme_RM6785-common device/realme/RM6785-common
-git clone --depth=1 https://github.com/plrine/device_realme_RMX2001L1 device/realme/RMX2001L1
-git clone --depth=1 https://github.com/plrine/proprietary_vendor_realme_RM6785-common vendor/realme/RM6785-common
+rm -rf */xiaomi
+git clone --depth=1 https://github.com/plrine/android_device_xiaomi_pond device/xiaomi/pond
+git clone --depth=1 https://github.com/plrine/proprietary_vendor_xiaomi_pond vendor/xiaomi/pond
 
-git clone --depth=1 https://github.com/halcyonproject/hardware_mediatek hardware/mediatek -b 15.1
-git clone --depth=1 https://github.com/halcyonproject/device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr -b 15.1
+git clone --depth=1 https://github.com/LineageOS/android_hardware_mediatek hardware/mediatek -b lineage-21.0
+git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi -b lineage-21.0
+git clone --depth=1 https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr -b lineage-21.0
 
 #beelding
 . build/envsetup.sh
-lunch halcyon_RMX2001L1-ap4a-user
-make carthage -j$(nproc --all)
+lunch lineage_pond-ap3a-eng
+make bacon -j$(nproc --all)
