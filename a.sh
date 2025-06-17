d83a77d5d6a80228c9560d0b6731f92575256017
Raya Mahardika <rayapkun@hlcyn.co>
2025-06-18 02:29:12 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -9,9 +9,9 @@
 
 # trees
 rm -rf */xiaomi
-git clone --depth=1 https://github.com/plrine/android_device_xiaomi_pond device/xiaomi/pond
-git clone --depth=1 https://github.com/plrine/device_xiaomi_pond-kernel device/xiaomi/pond-kernel
-git clone --depth=1 https://github.com/plrine/proprietary_vendor_xiaomi_pond vendor/xiaomi/pond
+git clone --depth=1 https://github.com/plrine/device_xiaomi_pond device/xiaomi/pond -b lineage-21
+git clone --depth=1 https://github.com/plrine/device_xiaomi_pond-kernel device/xiaomi/pond-kernel -b lineage-21
+git clone --depth=1 https://github.com/plrine/vendor_xiaomi_pond vendor/xiaomi/pond -b lineage-21
 
 git clone --depth=1 https://github.com/LineageOS/android_hardware_mediatek hardware/mediatek -b lineage-21
 git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi -b lineage-21
