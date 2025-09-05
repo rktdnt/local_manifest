a8c4994e46119de898b5da2ee8beb2c615c01a05
Raya Mahardika <rayapkun@hlcyn.co>
2025-09-05 22:27:49 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,27 +1,20 @@
 #
-repo init -u https://github.com/LineageOS/android.git -b lineage-22.1 --git-lfs
+repo init -u https://github.com/pos-fog/manifest.git -b fifteen --git-lfs
 /opt/crave/resync.sh
 
 # signing
 #rm -rf vendor/lineage-priv/keys
 #git clone --depth=1 https://github.com/rktdnt/p3 vendor/lineage-priv/keys
 
-#rm -rf vendor/aosp/signing/keys
-#git clone https://github.com/rktdnt/p3 -b pos vendor/aosp/signing/keys
-
-# wlan
-#rm -rf hardware/qcom-caf/wlan
-#git clone --depth=1 https://github.com/plrine/android_hardware_qcom_wlan hardware/qcom-caf/wlan
+rm -rf vendor/aosp/signing/keys
+git clone https://github.com/rktdnt/p3 -b pos vendor/aosp/signing/keys
 
 # trees
 rm -rf */xiaomi
-git clone --depth=1 https://github.com/plrine/android_device_xiaomi_msm8953-common device/xiaomi/msm8953-common
-git clone --depth=1 https://github.com/plrine/proprietary_vendor_xiaomi_msm8953-common vendor/xiaomi/msm8953-common
-git clone --depth=1 https://github.com/plrine/android_device_xiaomi_tissot device/xiaomi/tissot
-git clone --depth=1 https://github.com/plrine/proprietary_vendor_xiaomi_tissot vendor/xiaomi/tissot
-git clone --depth=1 https://github.com/plrine/android_kernel_xiaomi_msm8953 kernel/xiaomi/msm8953
-git clone --depth=1 https://github.com/plrine/hardware_xiaomi hardware/xiaomi
-git clone --depth=1 https://github.com/LineageOS/android_hardware_sony_timekeep hardware/sony/timekeep
+git clone --depth=1 https://github.com/plrine/android_device_xiaomi_surya device/xiaomi/surya
+git clone --depth=1 https://github.com/plrine/proprietary_vendor_xiaomi_surya vendor/xiaomi/surya
+git clone --depth=1 https://github.com/LineageOS/android_kernel_xiaomi_surya kernel/xiaomi/surya
+git clone --depth=1 https://github.com/pos-gm/android_hardware_xiaomi hardware/xiaomi
 
 # patches
 #cd vendor/aosp
@@ -30,5 +23,5 @@ git clone --depth=1 https://github.com/LineageOS/android_hardware_sony_timekeep 
 
 #beelding
 . build/envsetup.sh
-lunch lineage_tissot-ap3a-userdebug
+lunch aosp_surya-bp1a-user
 mka bacon -j$(nproc --all)
