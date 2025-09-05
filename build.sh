16312464ef4f0c34e35ed9c410383efdd973eec5
Raya Mahardika <rayapkun@hlcyn.co>
2025-09-05 22:30:04 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@
 set -e
 #Credit to Meghthedev for the initial script 
 # Initialize repo with specified manifest
-repo init -u https://github.com/accupara/los22.git -b lineage-22.0 --git-lfs
-crave set --projectID 93
+repo init -u https://github.com/PixelOS-AOSP/manifest -b fifteen --git-lfs
+crave set --projectID 82
 
-crave run --no-patch -- "curl -fsSL https://raw.githubusercontent.com/rktdnt/local_manifest/tissot/a.sh | bash" 
+crave run --no-patch -- "curl -fsSL https://raw.githubusercontent.com/rktdnt/local_manifest/surya/a.sh | bash" 
 
 # Clean up
-rm -rf tissot/
+rm -rf surya/
 
 # Pull generated zip files
 crave pull out/target/product/*/*.zip 
