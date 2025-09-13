04518b2c7e848f7b46ef269a6d725d9fa744014b
Raya Mahardika <rayapkun@hlcyn.co>
2025-09-13 23:11:43 +0700

    Update repository URLs and build commands in a.sh

--- a.sh
+++ a.sh
@@ -1,19 +1,19 @@
 #
-repo init -u https://github.com/plrine/manifest -b fifteen --git-lfs
+repo init -u https://github.com/RisingOS-Revived/android -b qpr2 --git-lfs
 /opt/crave/resync.sh
 
 # signing
-#rm -rf vendor/lineage-priv/keys
-#git clone --depth=1 https://github.com/rktdnt/p3 vendor/lineage-priv/keys
+rm -rf vendor/lineage-priv/keys
+git clone --depth=1 https://github.com/rktdnt/p3 vendor/lineage-priv/keys
 
-rm -rf vendor/aosp/signing/keys
-git clone https://github.com/rktdnt/p3 -b pos vendor/aosp/signing/keys
+#rm -rf vendor/aosp/signing/keys
+#git clone https://github.com/rktdnt/p3 -b pos vendor/aosp/signing/keys
 
 # trees
 rm -rf */xiaomi
 git clone --depth=1 https://github.com/plrine/android_device_xiaomi_surya device/xiaomi/surya
 git clone --depth=1 https://github.com/plrine/proprietary_vendor_xiaomi_surya vendor/xiaomi/surya
-git clone --depth=1 https://github.com/LineageOS/android_kernel_xiaomi_surya kernel/xiaomi/surya
+git clone --depth=1 https://github.com/aosp-surya/kernel_xiaomi_surya kernel/xiaomi/surya
 git clone --depth=1 https://github.com/plrine/android_hardware_xiaomi hardware/xiaomi
 
 # patches
@@ -23,5 +23,5 @@ git clone --depth=1 https://github.com/plrine/android_hardware_xiaomi hardware/x
 
 #beelding
 . build/envsetup.sh
-lunch aosp_surya-bp1a-user
-mka bacon -j$(nproc --all)
+riseup surya user
+rise b
